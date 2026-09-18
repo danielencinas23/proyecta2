be3d2059e36b4ecb2f1bae7dde8331d623bde6f3
Daniel <leviencinas23@gmail.com>
2026-09-18 17:09:54 -0600

    first commit

---
+++ proyectado/src/main/java/com/proyectado/ProyectadoApplication.java
@@ -0,0 +1,13 @@
+package com.proyectado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectadoApplication.class, args);
+	}
+
+}
---
+++ proyectado/src/test/java/com/proyectado/ProyectadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
